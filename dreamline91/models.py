10f0785d57baad169534439254d0e63aa32765a6
jaejunha <dreamline91@naver.com>
2018-01-09 00:03:10 +0900

    make db model(recommit because of time error)

--- dreamline91/models.py
+++ dreamline91/models.py
@@ -2,31 +2,26 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class User(models.Model):
-    user = models.CharField(max_length=20,primary_key=True)
-    name = models.CharField(max_length=20)
-    birth = models.IntegerField()
-    picture = models.CharField(max_length=100, null=True, default='')
-    def __str__(self):
-        return self.name
-
-class Hobby(models.Model):
-    name = models.CharField(max_length=20,primary_key=True)
-    user = models.CharField(max_length=20)
-    def __str__(self):
-        return self.name
-
-class School(models.Model):
-    name = models.CharField(max_length=20,primary_key=True)
-    type = models.CharField(max_length=20)
-    major = models.CharField(max_length=20)
-    score = models.IntegerField(null=True)
-    user = models.CharField(max_length=20)
-    def __str__(self):
-        return self.name
-
 class Favorite(models.Model):
     name = models.CharField(max_length=20)
     url = models.CharField(max_length=50, primary_key=True)
     def __str__(self):
         return self.name
+
+class Personal(models.Model):
+    bold = models.CharField(max_length=50, primary_key=True)
+    content = models.CharField(max_length=100)
+    def __str__(self):
+        return self.bold
+
+class History(models.Model):
+    start = models.IntegerField(primary_key=True)
+    finish = models.IntegerField(default=0)
+    content = models.CharField(max_length=100)
+    def __str__(self):
+        return self.start
+
+class HContent(models.Model):
+    year = models.IntegerField(default=0)
+    bold = models.CharField(max_length=20)
+    content = models.CharField(max_length=100)
