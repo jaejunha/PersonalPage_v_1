21bfd027e657239a7d02e3f19f054629a9a59421
jaejunha <dreamline91@naver.com>
2018-01-20 01:04:13 +0900

    refactoring a little & fix blink bug

--- static/js/home/frame.js
+++ static/js/home/frame.js
@@ -1,3 +1,71 @@
+var init = 1;
+var from = 0;//0:activity1:favorite
+var blink = 0;//activity button blink
+var switch_blink;//blink switch
+
+$(document).ready( function() {
+	var width = $(window).width();
+	$('#intro_content').load('home/intro');
+	$('#favorite_content').load('home/favorite');
+	$('#favorite_content').css('position','absolute');
+	$('#favorite_content').css('left',width+'px');
+	$('#activity_content').load('home/activity');
+	$('#activity_content').css('position','absolute');
+	$('#activity_content').css('left',width+'px');
+	$('#activity_content').css('width',(width-200)+'px');
+
+	var height = $(window).height() - $('#activity_content').offset().top;
+	$('#activity_content').css('height',height+'px');
+	$('#favorite_content').css('height',height+'px');
+
+	if(window.Worker){
+		var w = new Worker("{% static 'js/home/worker.js' %}?ver=0.1");
+        	w.onmessage = function(event) {
+			clearInterval(switch_blink);
+			if(init == 1 && home >= 0)
+			from=switch_screen(init--,from,'activity');
+        	};
+		w.postMessage("Hello");
+	}
+	if(from == 0)
+		resize('frame');
+
+	//delete animation
+	if(home != 1){
+		$('#favorite').css('animation','appear_tab 0s');
+		$('#favorite').css('opacity','1');
+		$('#activity').css('animation','appear_tab 0s');
+		$('#activity').css('opacity','1');
+		$('hr').css('animation','appear_tab 0s');
+		$('hr').css('opacity','1');	
+		$('#title').css('animation','appear_tab 0s');
+		$('#bird').css('animation','appear_bird 0s');
+		clearInterval(switch_blink);
+	}else{
+		switch_blink = setInterval(function() {
+		blink++;
+		if(blink % 2 == 1)
+			$('#activity').css('color','#ffffff');
+		else
+			$('#activity').css('color','#aaaaaa');
+		}, 750);
+	}
+	home--;
+});
+$(window).resize(function (){
+	if(init == 1){
+		var width = $(window).width();
+		$('#favorite_content').css('left',width+'px');
+		$('#activity_content').css('left',width+'px');
+		$('#activity_content').css('width',(width-200)+'px');
+
+		var height = $(window).height() - $('#activity_content').offset().top;
+		$('#activity_content').css('height',height+'px');
+	}
+
+	if(from == 0)
+		resize('frame');
+});
 function switch_screen(init,from,to){
 	var size = $(window).width();
 	var margin = 100;
