15fe2cfca57af464d5bbe490b61aa3bcbda9a876
jaejunha <dreamline91@naver.com>
2018-05-23 23:42:56 +0900

    resize header

--- static/js/index.js
+++ static/js/index.js
@@ -1,29 +1,51 @@
 var home = 1;
 var string_subMenu;
 $(document).ready( function() {
+	checkMobile();
+	showIntro();
+	resizeHeader();
+});
+
+$(window).resize(function() {
+	resizeHeader();
+});
+
+function checkMobile(){
 	var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
 	if(isMobile)
 		location.href='mobile';
-		switchMenu('home');
-		var bar = new ProgressBar.Circle(div_container, {
-			strokeWidth: 6,
-  			easing: 'easeInOut',
-  			duration: 1000,
-  			color: '#FFFFFF',
-			trailColor: '#555',
-			trailWidth: 2,
-  			svgStyle: null
-		});
-		
-		bar.animate(1.0);
-		setTimeout(function(){$("#div_screen").remove();}, 1500);
+}
 
-		var int_menuWidth = $(window).width()-20; 
-		$('header').css('width',int_menuWidth);
-		for(var i=1;i<=7;i++)
-			$('.li_outerMenu:nth-of-type('+i+')').css('width',$('.li_outerMenu:nth-of-type('+i+')').width()+85);
+function showIntro(){
+	switchMenu('home');
+	var bar = new ProgressBar.Circle(div_container, {
+		strokeWidth: 6,
+  		easing: 'easeInOut',
+  		duration: 1000,
+  		color: '#FFFFFF',
+		trailColor: '#555',
+		trailWidth: 2,
+  		svgStyle: null
+	});
 		
-});
+	bar.animate(1.0);
+	setTimeout(function(){$("#div_screen").remove();}, 1500);
+}
+
+function resizeHeader(){
+	var int_menuWidth = $(window).width()-20;
+	var int_contentWidth = (int_menuWidth - 100) / 7;
+	var int_outerFontSize = $(window).width() * 0.009 + 1;
+	var int_innerFontSize = $(window).width() * 0.007 + 1;
+
+	$('header').css('width',int_menuWidth);
+	$('.li_outerMenu').css('font-size', int_outerFontSize+'pt');
+	$('.li_innerMenu').css('font-size', int_innerFontSize+'pt');
+	for(var i=1;i<=7;i++){
+		$('.li_outerMenu:nth-of-type('+i+')').css('width', int_contentWidth);
+	}
+}
+
 function switchMenu(to,sub){
 
 	string_subMenu = '';
