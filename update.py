3e15aa746fa3cf02ffa5a058a0266ab4ff1b6b29
jaejunha <dreamline91@naver.com>
2018-06-26 14:19:00 +0900

    use resquests library instead of using urllib library

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8
+﻿# -*- coding: utf-8 -*-
 import requests
 import time
 import threading
@@ -6,7 +6,6 @@ import sqlite3
 import re
 import os
 import sys
-import urllib
 
 from selenium import webdriver 
 from PIL import Image
@@ -37,18 +36,17 @@ if __name__ == '__main__':
 def reset_url(old_url):
 	url = old_url
 	if url:
-                del url[:]
-        cursor.execute('select * from dreamline91_favorite')
-        rows = cursor.fetchall()
-        for r in rows:
-                url.append(r[1])
+		del url[:]
+	cursor.execute('select * from dreamline91_favorite')
+	rows = cursor.fetchall()
+	for r in rows:
+		url.append(r[1])
 	return url
 
 def process_url(u,h):
 	t = h[6:].replace('"','')
 	if t.find('http') <0:
                 t = u+h[6:].replace('"','')
-
 	return t
 
 def get_favicon(u,i):
@@ -57,56 +55,40 @@ def get_favicon(u,i):
 		t = t[:t.rfind('/')]
 	t = t+'/favicon.ico'
 	if save_favicon(i,t) == False:
-		response = requests.get(u)
-		for l in link.findall(response.text):
+		for l in link.findall(requests.get(u).text):
 			if l.find('"icon"') >=0 or l.find('"shortcut icon"') >= 0:
 				if len(href.findall(l)) > 0:
 					t = process_url(u,href.findall(l)[0])
 					if save_favicon(i,t) == True:
 						break
 def save_favicon(i,t):
-	image = urllib.urlopen(t)
-	down = file('static/site/'+str(i), "wb")
-	sum = 0
-	length = 0
+	image = requests.get(t, stream=True, allow_redirects=True)
 	ok = True
-
-	while True:
-		buf = image.read(100000000)
-		
-		if buf.find('html') >= 0:
-			ok = False
-			break
-		
-		length = len(buf)
-		sum += length
-
-		if length == 0:
-			break
-		
-		down.write(buf)
-	down.close()
-	image.close()
-	if sum == 0:
+	if image.headers['Content-length'] == '0':
 		ok = False
+	if image.status_code != 200:
+		ok = False		
+	if ok == True:
+		open('static/site/'+str(i), 'wb').write(image.content)
+	del image
 	return ok
 
 def save_capture(browser,u,i):
-	print 'working at '+u
 	browser.get(u)
 	time.sleep(5)
-        browser.save_screenshot("static/site/"+str(i)+".png")
-       	im = Image.open('static/site/'+str(i)+'.png')
-        if im.size[0]<=im.size[1]:
-        	min = im.size[0]
-                box = (0, 0, min, min)
+	browser.save_screenshot("static/site/"+str(i)+".png")
+	im = Image.open('static/site/'+str(i)+'.png')
+	if im.size[0]<=im.size[1]:
+		min = im.size[0]
+		box = (0, 0, min, min)
 		region = Image.new("RGBA", (min,min), (255,255,255,255))
 		region.paste(im.crop(box),mask=im.crop(box))
-        else:
-                min = im.size[1]
-               	region = Image.new("RGBA", (im.size[0], im.size[0]), (255,255,255,32))
+	else:
+		min = im.size[1]
+		region = Image.new("RGBA", (im.size[0], im.size[0]), (255,255,255,32))
 		temp = Image.new("RGBA", (im.size[0], im.size[1]), (255,255,255,255))
-                region.paste(temp,mask=temp)
+		region.paste(temp,mask=temp)
 		region.paste(im, (0,0,im.size[0],im.size[1]),mask=im)
-        region.thumbnail((min/5, min/5))
-        region.save('static/site/Website'+str(i)+'.png')
+	region.thumbnail((min/5, min/5))
+	region.save('static/site/Website'+str(i)+'.png')
+	print 'finish work at '+u
