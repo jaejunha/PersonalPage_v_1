608c2cf053bde5f2b525996d6ef5a120e96c417e
jaejunha <dreamline91@naver.com>
2018-06-30 22:05:36 +0900

    update favorite icon everyday

--- static/js/home/favorite.js
+++ static/js/home/favorite.js
@@ -14,40 +14,38 @@ function resize(){
 	int_width = $(window).width()-2*int_margin;
 	$('#p_main').css('width',int_width);
 
-	$('a').css('text-decoration','none');
+	$('.div_icon').css('position','relative');
+	$('.div_icon').css('width',0.16*int_width);
+	$('.div_icon').css('height',0.15*int_width);
+	$('.div_icon').css('float','left');
+	$('.div_icon').css('display','block');
+	$('.div_icon').css('background-color','rgba(255,255,255,0.2)');
+	$('.div_icon').css('overflow','hidden');
 
-	$('.icon').css('position','relative');
-	$('.icon').css('width',0.16*int_width);
-	$('.icon').css('height',0.15*int_width);
-	$('.icon').css('float','left');
-	$('.icon').css('display','block');
-	$('.icon').css('background-color','rgba(255,255,255,0.2)');
-	$('.icon').css('overflow','hidden');
+	$('.div_desc').css('width',0.16*int_width);
+	$('.div_desc').css('height',0.03*int_width);		
+	$('.div_desc').css('position','absolute');
+	$('.div_desc').css('top',0.12*int_width);
+	$('.div_desc').css('font-size',0.008*int_width+'pt');
 
-	$('.desc').css('width',0.16*int_width);
-	$('.desc').css('height',0.03*int_width);		
-	$('.desc').css('position','absolute');
-	$('.desc').css('top',0.12*int_width);
-	$('.desc').css('font-size',0.008*int_width+'pt');
+	$('.div_gradation').css('width',0.16*int_width);
+	$('.div_gradation').css('height',0.12*int_width);		
+	$('.div_gradation').css('position','absolute');	
+	$('.div_gradation').css('top',0);	
+	$('.div_gradation').css('background','-webkit-linear-gradient(top, rgba(255,255,255,0.8) 0%,rgba(255,255,255,0) 100%)');
+	$('.div_gradation').css('z-index','1');
 
-	$('.gradation').css('width',0.16*int_width);
-	$('.gradation').css('height',0.12*int_width);		
-	$('.gradation').css('position','absolute');	
-	$('.gradation').css('top',0);	
-	$('.gradation').css('background','-webkit-linear-gradient(top, rgba(255,255,255,0.8) 0%,rgba(255,255,255,0) 100%)');
-	$('.gradation').css('z-index','1');
-
-	$('.icon img').css('width',0.10*int_width);
-	$('.icon img').css('height',0.10*int_width);
-	$('.icon img').css('position','absolute');
-	$('.icon img').css('top',0.025*int_width);	
-	$('.icon img').css('left',0.03*int_width);	
-	$('.icon img').css('z-index','-1');	
-	$('.icon img').css('display','block');	
+	$('.img_screenshot').css('width',0.10*int_width);
+	$('.img_screenshot').css('height',0.10*int_width);
+	$('.img_screenshot').css('position','absolute');
+	$('.img_screenshot').css('top',0.025*int_width);	
+	$('.img_screenshot').css('left',0.03*int_width);	
+	$('.img_screenshot').css('z-index','-1');	
+	$('.img_screenshot').css('display','block');	
 
 	//must locate behind icon img
-	$('.favi').css('left',0.005*int_width);
-	$('.favi').css('top',0.005*int_width);
-	$('.favi').css('width',0.02*int_width);
-	$('.favi').css('height',0.02*int_width);
+	$('.img_favicon').css('left',0.005*int_width);
+	$('.img_favicon').css('top',0.005*int_width);
+	$('.img_favicon').css('width',0.02*int_width);
+	$('.img_favicon').css('height',0.02*int_width);
 }
